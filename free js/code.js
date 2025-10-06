d0fb2458c5218f91bc037ed4b39d330fe2b74d60
Pavel Mitrofanov <mitrof4nov.p@gmail.com>
2025-10-06 12:26:16 +0300

    new file:   labsJS/lab2/lab2.1.html 	new file:   labsJS/lab2/lab2.2.html 	new file:   labsJS/lab2/lab2.3.html

---
+++ free js/code.js
@@ -0,0 +1,13 @@
+let name = prompt("What is your name");
+let shouldContinue = confirm("do u have two numbers?")
+if (shouldContinue){
+    let x = prompt('x');
+    let y = prompt('y');
+    alert(parseInt(x)+parseInt(y));  
+    document.write('idk')
+}
+else{
+    alert("byel")
+}
+console.log(" i'm here ");
+alert(`Welcome ${name}`);
